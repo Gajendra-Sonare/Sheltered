97729e057f5e5daa08a30c03b9a0a355bd95fc78
Gajendra <gajendrasonare@gmail.com>
2022-06-05 16:07:54 +0530

    can view use post

--- src/private/mypost.jsx
+++ src/private/mypost.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {useContext, useState} from 'react';
+import {useContext, useState, useEffect} from 'react';
 import authContext from '../utils/auth-context';
 import {useNavigate} from 'react-router-dom';
 import axios from 'axios'; 
+import PrivateNavbar from '../privateComponents/privateNavbar';
 
 function showContent(info){
     console.log('info', info);
@@ -13,13 +14,23 @@ function showContent(info){
             //     }
 
         let temp = ["http://127.0.0.1:8000/media/post_image/IMG-20220429-WA0011.jpg", "http://127.0.0.1:8000/media/post_image/android-chrome-192x192_5nS20k8.png"];
-
+        let url = "http://127.0.0.1:8000"
 
         //{allid.map(id=>{<ol>{allpics[id].map(pic=>{<li><img width={200} height={200} src={pic}/></li>})}</ol>})} 
         return(
             <div>
-            
-                
+                {info.map( post =>
+                    <div>
+                        <h1>{"post id: "+ post[0]}</h1>
+                        <h1>{"title: " + post[1]}</h1>
+                        <h1>{"price: " + post[2]}</h1>
+                        <h1>{"description: " + post[3]}</h1>
+                        <img width={200} height={200} src={url + post[4]}/>
+                        { post[5] ? <img width={200} height={200} src={url + post[5]}/> : console.log(0)}
+                        { post[6] ? <img width={200} height={200} src={url + post[6]}/> : console.log(0)}
+                        { post[7] ? <img width={200} height={200} src={url + post[7]}/> : console.log(0)}
+                    </div>
+                )}
             </div>
         )
     }
@@ -57,8 +68,11 @@ const MyPost = () =>{
     }
     return (
         <div>
-            <h1>My Post</h1> 
-            <button onClick={submit}>submit</button>
+            <PrivateNavbar/>
+            {/* <button onClick={submit}>submit</button> */}
+            {useEffect(()=>{
+                submit();
+            }, [])}
             {show && showContent(info)}
         </div>
     )
